7551ef0b1d0ac27593761317c04ec84ee8726b71
laurieta <laurengiulia15@outlook.com>
2024-06-05 18:25:45 -0300

    new version from lauren

---
+++ teste.js
@@ -0,0 +1,7 @@
+document.getElementById('contentForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Previne o comportamento padrão do formulário
+
+    // Realize qualquer validação ou ação adicional aqui
+
+    window.location.href = 'resenhas2.html'; // Redireciona para a página desejada
+});
